603dbcf84139fecace24fec8bd96a5262197569d
Chencxcx <2641747553>
2017-12-13 14:42:30 +0800

    1213

---
+++ Lithography/app/src/main/java/com/bwie/lithography/activity/IntoActivity.java
@@ -0,0 +1,15 @@
+package com.bwie.lithography.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bwie.lithography.R;
+
+public class IntoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_into);
+    }
+}
